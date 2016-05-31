bba6de104f06b208069479e744d62ff68ea12408
Aaron Rice <adrice727@gmail.com>
2016-05-31 12:58:20 -0700

    Add credentials endpoint.

--- services/api.js
+++ services/api.js
@@ -1,12 +1,12 @@
 'use strict';
 // Config
 const config = require('../config/config');
-const apiKey = config.apiKey;
-const apiSecret = config.apiSecret;
-
+const apiKey = process.env.API_KEY || config.apiKey;
+const apiSecret = process.env.API_SECRET || config.apiSecret;
 
 // Imports
 const Promise = require('bluebird');
+const async = Promise.coroutine;
 const R = require('ramda');
 const OpenTok = require('opentok');
 const OT = Promise.promisifyAll(new OpenTok(apiKey, apiSecret));
@@ -16,7 +16,7 @@ const defaultTokenOptions = {
   expireTime: (Date.now() / 1000) + (7 * 24 * 60 * 60), // in one week
 };
 
-const createSession = function*(options) {
+const createSession = function* (options) {
   const session = yield OT.createSessionAsync(options);
   yield session;
 };
@@ -29,10 +29,23 @@ const getSession = function* () {
 const getToken = function* () {
   const sessionId = this.request.body.sessionId;
   const tokenOptions = R.merge(defaultTokenOptions, R.path(['request', 'body', 'options'], this));
+
+  if (!sessionId) {
+    this.throw(403, 'An OpenTok session id is required to generate a token');
+  }
+
   this.body = OT.generateToken(sessionId, tokenOptions);
 };
 
+const getCredentials = async(function* () {
+  const session = yield OT.createSessionAsync();
+  const token = OT.generateToken(session.sessionId);
+  this.body = { apiKey, session, token };
+});
+
+
 module.exports = {
+  getCredentials,
   createSession,
   getSession,
   getToken,
